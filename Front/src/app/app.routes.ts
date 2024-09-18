7fdcae489713624ad093c3434f173b51b20fb90b
davidongora <davidongora@gmail.com>
2024-09-18 21:14:59 +0300

    set routes to landing page

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { CustomersComponent } from './home/customers/customers.component';
+import { provideHttpClient } from '@angular/common/http'; // Import provideHttpClient
+import { provideRouter } from '@angular/router';
 
 export const routes: Routes = [
   
@@ -10,8 +13,19 @@ export const routes: Routes = [
     path: 'store',
     loadChildren: () => import('./store/store.module').then(m => m.StoreModule)
   },
+  { path: 'customers', component: CustomersComponent } ,// Define the route for CustomersComponent
+
   {
     path: '**',
-    redirectTo: '/home/info'
+    redirectTo: 'home/landing'
   },
+
+  
 ];
+
+export const appConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient() // Provide HttpClient globally
+  ]
+};
