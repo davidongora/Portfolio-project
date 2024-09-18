e4a7e6099bb0f06b1ecb2b3d389db4793c5d305b
davidongora <davidongora@gmail.com>
2024-09-18 21:19:52 +0300

    faqs to be updated

--- Front/src/app/home/faqs/faqs.component.ts
+++ Front/src/app/home/faqs/faqs.component.ts
@@ -1,21 +1,24 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-faqs',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './faqs.component.html',
   styleUrl: './faqs.component.css'
 })
 export class FaqsComponent {
-  private expandedItemId: string | null = null;
+  
+  faqs = [
+    { question: "What is Angular?", answer: "Angular is a platform and framework for building single-page client applications using HTML and TypeScript.", show: false },
+    { question: "How do I install Angular?", answer: "You can install Angular using the Angular CLI by running `npm install -g @angular/cli` in your terminal.", show: false },
+    { question: "What is a component in Angular?", answer: "A component controls a patch of the screen called a view and consists of HTML, CSS, and TypeScript files.", show: false },
+  ];
 
-  toggleCollapse(id: string): void {
-    this.expandedItemId = this.expandedItemId === id ? null : id;
+  toggleFAQ(index: number): void {
+    this.faqs[index].show = !this.faqs[index].show;
   }
 
-  isExpanded(id: string): boolean {
-    return this.expandedItemId === id;
-  }
 }
