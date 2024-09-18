82dbffccc5201b06473c3047b7c978b3b0b2a956
davidongora <davidongora@gmail.com>
2024-09-18 21:19:19 +0300

    displays list of customers

--- Front/src/app/home/customers/customers.component.ts
+++ Front/src/app/home/customers/customers.component.ts
@@ -1,15 +1,31 @@
+import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { ApiService } from '../../api.service'; // Make sure the path is correct
+import { first, Subscription } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, HttpClientModule],
+  
   templateUrl: './customers.component.html',
-  styleUrl: './customers.component.css'
+  styleUrls: ['./customers.component.css'] // Fixed typo 'styleUrl' to 'styleUrls'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
 
-  images : any[] = ['image1', 'image2', 'image3', 'image4',' image5','image6']
+  images : any[] = ['NextFashion', 'FashionForAll', 'Queen clozet', 'FahionForAll',' Queen clozet']
+  products: any[] = []; // List to hold fetched products
+  isLoading: boolean = false;
+
+  private unsubscribe: Subscription[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+
+  ngOnInit(): void {
+  }
 
 }
+
