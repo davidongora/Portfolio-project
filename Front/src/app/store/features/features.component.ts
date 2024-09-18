85f2216e7d7a3ea486c1545fa6d04cbe2177c818
davidongora <davidongora@gmail.com>
2024-09-18 21:22:55 +0300

    fetches products and displays

--- Front/src/app/store/features/features.component.ts
+++ Front/src/app/store/features/features.component.ts
@@ -1,27 +1,53 @@
-import { FilterComponent } from '../filter/filter.component';
-import { Component, OnInit, NgModule } from '@angular/core';
-import { NgFor } from '@angular/common';
-import { ApiService } from '../../api.service'; // Make sure the path is correct
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ApiService } from '../../api.service'; // Ensure the path is correct
 import { first, Subscription } from 'rxjs';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NgFor, NgIf } from '@angular/common';
+import { FilterComponent } from '../filter/filter.component';
 
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [FilterComponent, NgFor],
+  imports: [FilterComponent, NgFor, NgIf, MatDialogModule, MatButtonModule],
   templateUrl: './features.component.html',
-  styleUrl: './features.component.css'
+  styleUrls: ['./features.component.css'] // Ensure the styleUrl is correct
 })
-export class FeaturesComponent {
+export class FeaturesComponent implements OnInit {
   products: any[] = []; 
-  isLoading: boolean = false;
+  isLoading: boolean = true;
 
   private unsubscribe: Subscription[] = [];
   pageSize = 6; 
   currentPage = 1;
   pagedProducts: any[] = [];
-  constructor(private apiService: ApiService, ) {
-    this.updatePagedProducts();
+
+  constructor(private apiService: ApiService, public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.productslist(); 
+  }
+
+  productslist = () => {
+    this.isLoading = true;
+    console.log("Fetching products...");
+
+    const handoversub = this.apiService.getProducts()
+      .pipe(first())
+      .subscribe({
+        next: (response: any) => {
+          this.products = response;
+          this.updatePagedProducts();
+          this.isLoading = false;
+        },
+        error: (err) => {
+          this.isLoading = false;
+          console.error(err);
+        }
+      });
+
+    this.unsubscribe.push(handoversub);
   }
 
   updatePagedProducts() {
@@ -39,27 +65,17 @@ export class FeaturesComponent {
     return Math.ceil(this.products.length / this.pageSize);
   }
 
-  ngOnInit(): void {
-    this.productslist(); 
+  openDialog(templateRef: TemplateRef<any>, item: any): void {
+    this.dialog.open(templateRef, {
+      // data: item, // Passing selected product data to the modal
+      // width: '600px',
+      // height: '500px',
+      // panelClass: 'custom-dialog-container'
+    });
   }
+  
 
-  productslist = () =>{
-    this.isLoading = true;
-    console.log("function initiated");
-
-    const handoversub = this.apiService.getProducts()
-      .pipe(first())
-      .subscribe({
-        next: (response: any) => {
-          this.products = response;
-          this.updatePagedProducts(); 
-          // console.log(response);
-        },
-        error:(err) => {
-          this.isLoading = false;
-          console.log(err);
-        }
-      });
-    this.unsubscribe.push(handoversub);
+  closeDialog(): void {
+    this.dialog.closeAll(); // Close all dialogs
   }
 }
