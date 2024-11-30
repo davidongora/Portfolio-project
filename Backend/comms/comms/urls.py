6a5b0c1a1261c09f3d0d65b1dfa3e4f61a58a6e0
David Ongora <davidongora@gmail.com>
2024-11-30 04:47:22 -0800

    Update urls.py

--- Backend/comms/comms/urls.py
+++ Backend/comms/comms/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # path('swagger.json./', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # path('swagger-yaml/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # Swagger JSON
